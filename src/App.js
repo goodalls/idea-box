c6364e2601b7fe8124c244d355170cd9e9ec738e
Jeff Goodall <goodallhj@gmail.com>
2018-01-16 16:00:01 -0700

    added html and images folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import Header from "./Header.jsx";
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-       <h1>howdy -- hi</h1>
-      </div>
+      <wrapper>
+        <Header />
+
+        <section class="form">
+          <form>
+            <label for="title-input" />
+            <input
+              type="text"
+              id="title-input"
+              name="title-input"
+              required="true"
+              placeholder="Title"
+            />
+            <label for="body-input" />
+            <input
+              type="text"
+              id="body-input"
+              name="body-input"
+              required="true"
+              placeholder="Body"
+            />
+            <label for="save-button" />
+            <input
+              type="button"
+              value="save"
+              id="save-button"
+              disabled="true"
+            />
+            <label for="search-input" />
+            <input
+              type="text"
+              id="search-input"
+              name="search-input"
+              required="true"
+              placeholder="Search"
+            />
+          </form>
+        </section>
+
+        <section class="idea-section" />
+      </wrapper>
     );
   }
 }
-
-
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './css/Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="header-1">idea</h1>
+      <h1 className="header-2">box</h1>
+    </header>
+  )
+};
+
+export default Header;
+
+
+
+
+
